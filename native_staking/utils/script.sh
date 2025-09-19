58c35ac961c98dfedb802a091c42873dcbb7b734
dkdocs <deepak@topledger.xyz>
2025-09-19 16:18:30 +0530

    updates native staking

--- native_staking/utils/script.sh
+++ native_staking/utils/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-name=${1}
+name=${2}
+folder=${1}
 
 cd utils || exit
 ./convert < ../pkg/${name}_bg.wasm > wasm_bytes.js
-gsutil cp wasm_bytes.js gs://tt-bq-js/solana/${name}/wasm_bytes.js
-gsutil cp ../pkg/${name}.js gs://tt-bq-js/solana/${name}/
+gsutil cp wasm_bytes.js gs://tt-bq-js/solana/${folder}/${name}/wasm_bytes.js
+gsutil cp ../pkg/${name}.js gs://tt-bq-js/solana/${folder}/${name}/
