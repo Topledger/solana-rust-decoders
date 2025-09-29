ace9b17b0f7ab405d3cfc14fb43da59a51e977b8
Admin <admin@Admins-MacBook-Pro.local>
2025-09-29 16:58:50 +0530

    Fix merge conflicts in Makefile and script.sh

--- native_staking/utils/script.sh
+++ native_staking/utils/script.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-<<<<<<< HEAD
-name=${1}
+name=${2}
 
 cd utils || exit
 ./convert < ../pkg/${name}_bg.wasm > wasm_bytes.js
 gsutil cp wasm_bytes.js gs://tt-bq-js/solana/${name}/wasm_bytes.js
 gsutil cp ../pkg/${name}.js gs://tt-bq-js/solana/${name}/
-=======
-name=${2}
-folder=${1}
-
-cd utils || exit
-./convert < ../pkg/${name}_bg.wasm > wasm_bytes.js
-gsutil cp wasm_bytes.js gs://tt-bq-js/solana/${folder}/${name}/wasm_bytes.js
-gsutil cp ../pkg/${name}.js gs://tt-bq-js/solana/${folder}/${name}/
->>>>>>> 58c35ac961c98dfedb802a091c42873dcbb7b734
