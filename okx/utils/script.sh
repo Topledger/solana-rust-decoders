fbdc06206055a29369e61e1b64b30d808694f531
Admin <admin@Admins-MacBook-Pro.local>
2025-11-08 16:19:24 +0530

    Clean up okx folder - keep only okx_v1 and okx_v2 decoders

--- okx/utils/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-name=${2}
-
-cd utils || exit
-./convert < ../pkg/${name}_bg.wasm > wasm_bytes.js
-gsutil cp wasm_bytes.js gs://tt-bq-js/solana/${name}/wasm_bytes.js
-gsutil cp ../pkg/${name}.js gs://tt-bq-js/solana/${name}/
-
-
